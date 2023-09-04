5cbe4a0ef7fabce195fcc6e0bba04c09e392dcff
Кондратенко Александр <kondratenkoaa2@edu.mos.ru>
2023-09-04 21:36:08 +0300

    fix: Добавлено описание модели News

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,8 +2,32 @@
 
 namespace App\Models;
 
+use Database\Factories\NewsFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\News
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $text
+ * @property string $slug
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static NewsFactory factory(...$parameters)
+ * @method static Builder|News newModelQuery()
+ * @method static Builder|News newQuery()
+ * @method static Builder|News query()
+ * @method static Builder|News whereCreatedAt($value)
+ * @method static Builder|News whereId($value)
+ * @method static Builder|News whereSlug($value)
+ * @method static Builder|News whereText($value)
+ * @method static Builder|News whereTitle($value)
+ * @method static Builder|News whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class News extends Base
 {
     use HasFactory;
